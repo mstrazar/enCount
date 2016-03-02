dbb2b96da41059d4d8011635f4a013680585be56
mstrazar <martin.strazar@gmail.com>
2016-03-02 19:56:45 +0100

    Listing parameters in example myconfig.py file.

---
+++ enCount/myconfig.py
@@ -0,0 +1,6 @@
+
+NUM_THREADS = 4
+EXECUTABLE_STAR = "/usr/bin/STAR"                               # STAR aligner executable
+GENOME_DIR = "/n/users/martins/Dev/data/encode/genomes/"        # GenomesDir for STAR aligner
+GENOME_FASTA_DIR = "/n/users/martins/Dev/data/genome/hg19"  # GenomesFastaDir for STAR aligner
+
