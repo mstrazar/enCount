fb26c93c37e47f0f836fe9adc93a6f01b88b10df
mstrazar <martin.strazar@gmail.com>
2016-03-03 23:45:33 +0100

    Size factor calculation.

--- enCount/externals/qorts.R
+++ enCount/externals/qorts.R
@@ -1,10 +1,21 @@
 # Estimate size factors with QoRTs
-library("QoRTs")
 
+args = commandArgs(TRUE)
 in_dir     = args[1]    # directory contains raw count files
 in_decoder = args[2]    # decoder is the metadata file indicating sample IDs
 out_file   = args[3]    # size factor file to be generated
 
-res = read.qc.results.data(in_dir, decoder = in_decoder,
+
+print(in_dir)
+print(in_decoder)
+print(out_file)
+
+library("QoRTs")
+# directory <- paste0(system.file("extdata/", package="QoRTsExampleData", mustWork=TRUE),"/");
+
+directory = in_dir
+decoder.data = read.table(in_decoder, header=T, stringsAsFactors=F);
+
+res = read.qc.results.data(directory, decoder=decoder.data,
     calc.DESeq2 = TRUE, calc.edgeR = TRUE);
 get.size.factors(res, outfile=out_file);
