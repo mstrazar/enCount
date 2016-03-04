5ebad8b8c1eb0357d00c09cfd96fd74bfa4c3508
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2016-03-04 18:28:15 +0100

    Merge configs

--- enCount/myconfig.py
+++ enCount/myconfig.py
@@ -1,3 +1,6 @@
+data_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/data'
+genomes_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/genomes'
+results_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/results'
 
 NUM_THREADS = 4
 
