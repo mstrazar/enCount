0aaa6a7d7371b5aa3aff1baf01b840b2035f5a76
mstrazar <martin.strazar@gmail.com>
2016-03-04 19:31:47 +0100

    Delete my config from repository.

--- enCount/myconfig.py
+++ enCount/myconfig.py
@@ -1,17 +1,3 @@
 data_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/data'
 genomes_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/genomes'
 results_root = '/Users/tomazc/workspace/tmp_data/enCount/storage/results'
-
-NUM_THREADS = 4
-
-
-
-# STAR aligner
-STAR_EXECUTABLE = "/usr/bin/STAR"                               # STAR aligner executable
-GENOME_DIR = "/n/users/martins/Dev/data/encode/genomes/"        # GenomesDir for STAR aligner
-GENOME_FASTA_DIR = "/n/users/martins/Dev/data/genome/hg19"      # GenomesFastaDir for STAR aligner
-
-
-# QoRTS
-QORTS_MIN_COUNT = 6
-QORTS_EXECUTABLE = "/n/users/martins/Dev/lib/bioconductor-deseq/QoRTs.jar"
