d31e1895a70da16d9567a70a9e24da6086d6bb70
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2016-04-27 14:53:48 +0200

    Simplified metadata retrival

--- enCount/encode.py
+++ enCount/encode.py
@@ -5,6 +5,7 @@ Interface to query and download from ENCODE.
 
 import csv
 import requests
+import urllib
 
 HEADERS = {'accept': 'application/json'}
 
@@ -22,11 +23,10 @@ def parse_metadata_records(metadata_records):
     return exps
 
 
-def get_online_list(assay_titles=('RNA-seq', 'shRNA/RNA-seq'),
+def get_online_list(assay_titles=('RNA-seq', 'shRNA+RNA-seq'),
                     organisms=('Homo sapiens', ),
                     biosamples=('K562', 'HepG2')):
 
-    # get json with link to batch download file
     params = {
         'type': 'Experiment',
         'assay_title': assay_titles,
@@ -39,27 +39,14 @@ def get_online_list(assay_titles=('RNA-seq', 'shRNA/RNA-seq'),
         'frame': 'objects',
     }
 
-    print("Getting info on matching experiments...")
-    url = 'https://www.encodeproject.org/search/'
-    response = requests.get(url, params=params, headers=HEADERS)
-    print(response.url)
-    resp_json_dict = response.json()
-
-    # get link for batch download
-    batch_download_url = resp_json_dict['batch_download']
-
-    # get link to metadata file
-    response = requests.get(batch_download_url)
-    metadata_url = response.text.splitlines()[0]
-    host, query = metadata_url.split('/metadata/')
-    query = requests.utils.quote(query)
-    metadata_url = '{:s}/metadata/{:s}'.format(host, query)
-
-    # download metadata file
-    print("Downloading metadata file...")
-    response = requests.get(metadata_url)
+    print('Getting info on matching experiments...')
+    params = urllib.parse.urlencode(params, doseq=True)
+    params = urllib.parse.quote(params)
+    url = 'https://www.encodeproject.org/metadata/{:s}/metadata.csv'.format(
+        params)
+    response = requests.get(url)
     print(response.url)
     reader = csv.reader(response.text.splitlines(), delimiter='\t')
     metadata_records = [row for row in reader]
-
+    print('Retrieved {:d} records.'.format(len(metadata_records)))
     return parse_metadata_records(metadata_records)
