99258a221f5b4ec68df63559f088ee8c1f247dfc
mstrazar <martin.strazar@gmail.com>
2016-06-23 17:03:42 +0200

    Functions return output codes. Import PATHs from local environment config.

--- enCount/externals/junctionseq.py
+++ enCount/externals/junctionseq.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import subprocess
-import glob
-import os
+from enCount.config import QORTS_JAR, QORTS_R
 
 def run_QoRTs_count(in_bam, in_gtf, out_dir):
     """
@@ -22,15 +21,13 @@ def run_QoRTs_count(in_bam, in_gtf, out_dir):
         Write count.txt file in the output directory.
     """
 
-    # TODO: add QoRTs.jar to environment path
-    args = ["java", "-jar", "/usr/bin/QoRTs.jar",
+    args = ["java", "-jar", QORTS_JAR,
             "QC", "--stranded",
             in_bam, in_gtf,
             out_dir]
 
     print(" ".join(args))
-    subprocess.call(args)
-    return
+    return subprocess.call(args)
 
 
 def run_QoRTs_merge(in_dir, in_decoder, out_dir):
@@ -48,12 +45,11 @@ def run_QoRTs_merge(in_dir, in_decoder, out_dir):
         Merge count files given by the decoder and place in the output
         directory.
     """
-    args = ["java", "-jar", "/usr/bin/QoRTs.jar",
+    args = ["java", "-jar", QORTS_JAR,
             "mergeAllCounts", in_dir, in_decoder, out_dir]
 
     print(" ".join(args))
-    subprocess.call(args)
-    return
+    return subprocess.call(args)
 
 
 def run_QoRTs_size_factors(in_dir, in_decoder, out_file):
@@ -65,14 +61,10 @@ def run_QoRTs_size_factors(in_dir, in_decoder, out_file):
     :param out_file
         Size factor file to be generated
     """
-
-    # TODO: place qorts.R in environment path
-    args = ["Rscript", "/n/users/martins/Dev/enCount/enCount/externals/qorts.R",
+    args = ["Rscript", QORTS_R,
             in_dir, in_decoder, out_file]
-
     print(" ".join(args))
-    subprocess.call(args)
-    return
+    return subprocess.call(args)
 
 
 def run_QoRTs_novel_splices(in_dir, in_gtf, in_size_factors, out_dir,
@@ -95,11 +87,9 @@ def run_QoRTs_novel_splices(in_dir, in_gtf, in_size_factors, out_dir,
         Produce .gff file with novel splice junctions and
         updated count files in the output directory.
     """
-
-    args = ["java", "-jar", "/usr/bin/QoRTs.jar", "mergeNovelSplices",
+    args = ["java", "-jar", QORTS_JAR, "mergeNovelSplices",
                 "--minCount", str(min_count), "--stranded",
                 in_dir, in_size_factors, in_gtf, out_dir,]
 
     print(" ".join(args))
-    subprocess.call(args)
-    return
+    return subprocess.call(args)
