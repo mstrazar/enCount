5abff236248e71024e16e40eb0fd13d9056c52ca
mstrazar <martin.strazar@gmail.com>
2016-11-07 16:55:20 +0100

    Functional tests: Generate genome and FASTQ alignment.

--- enCount/tests/externals/rnastar.py
+++ enCount/tests/externals/rnastar.py
@@ -4,45 +4,54 @@ from enCount.externals.rnastar import run_star, run_star_generate_genome
 from enCount.config import data_root, genomes_root, results_root
 import shutil
 import unittest
+import subprocess
 
 class TestRNASTAR(unittest.TestCase):
 
-    def test_rnastar(self):
-        """
-        Testing the STAR aligner pipeline inside a container.
-        """
+    def test_rnastar_generate_genome(self):
         in_genome_fasta_dir = os.path.join(genomes_root, "fasta", "hg19")
-        in_gtf = os.path.join(genomes_root, "gtf", "Homo_sapiens.GRCh37.75.gtf")
-        in_fastq_1 = os.path.join(data_root, "ENCSRSAMPLE", "ENCFF018JDZ_filter.fastq.gz")
-        in_fastq_2 = os.path.join(data_root, "ENCSRSAMPLE", "ENCFF273CYL_filter.fastq.gz")
-        out_genome_dir = os.path.join(genomes_root, "index", "Homo_sapiens.GRCh37.75")
-        out_dir = os.path.join(results_root, "mappings", "ENCSRSAMPLE")
+        in_gtf = os.path.join(genomes_root, "gtf", "initial.gtf")
+        out_genome_dir = os.path.join(genomes_root, "index", "initial/")
         num_threads = 10
-
 
         print("Working directory", os.getcwd())
 
         # Check input files
-        for f_in in [in_genome_fasta_dir, in_gtf, in_fastq_1, in_fastq_2]:
+        for f_in in [in_genome_fasta_dir, in_gtf, out_genome_dir]:
+            print("\tChecking %s" % f_in)
             self.assertTrue(os.path.exists(f_in))
-
-        # Empty test folders
-        for d_out in [out_genome_dir, out_dir]:
-            if os.path.exists(d_out):
-                shutil.rmtree(d_out)
-            os.makedirs(d_out)
 
         # Generate genome
         r = run_star_generate_genome(in_gtf=in_gtf,
-                                 in_genome_fasta_dir=in_genome_fasta_dir,
-                                 out_genome_dir=out_genome_dir,
-                                 num_threads=num_threads)
+                                     in_genome_fasta_dir=in_genome_fasta_dir,
+                                     out_genome_dir=out_genome_dir,
+                                     num_threads=num_threads)
         self.assertEqual(r, 0)
+
+
+
+    def test_rnastar_align(self):
+        """
+        Testing the STAR aligner pipeline inside a container.
+        """
+        in_fastq_1 = os.path.join(data_root, "fastq", "ENCSRSAMPLE", "ENCFF018JDZ_filter.fastq.gz")
+        in_fastq_2 = os.path.join(data_root, "fastq", "ENCSRSAMPLE", "ENCFF273CYL_filter.fastq.gz")
+        in_genome_dir = os.path.join(genomes_root, "index", "initial/")
+        out_dir = os.path.join(results_root, "mappings", "ENCSRSAMPLE/")
+        num_threads = 10
+
+        print("Working directory", os.getcwd())
+
+        # Check input files
+        for f_in in [in_fastq_1, in_fastq_2, in_genome_dir, out_dir]:
+            print("\tChecking %s" % f_in)
+            self.assertTrue(os.path.exists(f_in))
 
         # Run alignment
         r = run_star(in_fastq_pair=[in_fastq_1, in_fastq_2],
-                 out_dir=out_dir, in_genome_dir=out_genome_dir,
+                 out_dir=out_dir, in_genome_dir=in_genome_dir,
                  num_threads=num_threads)
+
         self.assertEqual(r, 0)
 
 
