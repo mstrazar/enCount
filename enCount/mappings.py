1be73d4135561b7f62de15dab8d60bd07bcf84b1
mstrazar <martin.strazar@gmail.com>
2016-11-07 17:01:13 +0100

    Add fastq_pair to mappings.

--- enCount/mappings.py
+++ enCount/mappings.py
@@ -1,6 +1,7 @@
 import os
-
 import enCount
+import hashlib
+from bson.objectid import ObjectId
 
 # populate list with currently queued jobs
 submitted_mappings = dict(
@@ -8,19 +9,70 @@ submitted_mappings = dict(
 )
 
 
-def _update_dbrec_status(dbrec_id, new_status):
+def _update_dbrec_status(dbrec_id, new_status, new_bam):
     r = enCount.db.mappings.update_one(
-        {'_id': ObjectId(dbrec_id)}, {"$set": {'status': new_status}}
+        {'_id': ObjectId(dbrec_id)}, {"$set": {'status': new_status,
+            'out_bam_file': new_bam}}
     )
     if not r.acknowledged:
         print(' problems updating collection mappings record id: {'
               ':s}'.format(dbrec_id))
 
-def map_fastq(fastqs, gtf_ver, outbam_fname):
+
+
+def map_fastq(fastq_pair, gtf_ver, out_dir, dbrec_id):
+
+    in_genome_dir = enCount.gtfs.get_genome_index_dir(gtf_ver=gtf_ver)
+    enCount.externals.rnastar.run_star(
+        in_genome_dir=in_genome_dir,
+        in_fastq=fastq_pair,
+        out_dir=out_dir,
+    )
+
+    new_bam = os.path.join(out_dir, "Aligned.sortedByCoord.out.bam")
+    if os.path.exists(new_bam):
+        _update_dbrec_status(dbrec_id, 'ready', new_bam)
+    else:
+        _update_dbrec_status(dbrec_id, 'error', new_bam)
     return
 
 
-def get_bam_file_path(fastqs, gtf_ver):
+def get_bam_file_path(fastq_pair, gtf_ver):
+
+    """Return path to file or None if file not available."""
+    # query DB
+    mappings = enCount.db.mappings.find(
+        {'fastq_pair': fastq_pair, 'gtf_ver': gtf_ver}
+    )
+    assert(mappings.count() <= 1)
+    mappings = list(mappings)
+
+    if mappings:
+        # fetch record from DB
+        mapping = mappings[0]
+        if mapping['status'] == 'ready':
+            return mapping['bam_file_path']
+        else:
+            # not ready
+            return
+    else:
+        bname   = hashlib.md5(str(fastq_pair)).hexdigest()
+        out_dir = os.path.join(enCount.config.mappings_root, gtf_ver, bname)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        time_stamp = datetime.datetime.utcnow()
+        new_rec = {'fastq_pair': fastq_pair, 'gtf_ver': gtf_ver,
+                   'status': 'to map', 'time_stamp': time_stamp,
+                   'out_dir': out_dir,
+        }
+        print('adding new record to mappings collection: {:s}'.format(
+            str(new_rec)))
+        enCount.db.mappings.insert_one(new_rec)
+        # not ready
+        return
+
+
     return
 
 
