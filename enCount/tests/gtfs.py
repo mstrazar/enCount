5b3650cfbab59a4d9152167bcf86c3df30be8ea8
mstrazar <martin.strazar@gmail.com>
2016-11-14 17:30:34 +0100

    Tests using process() method across different containers.

--- enCount/tests/gtfs.py
+++ enCount/tests/gtfs.py
@@ -2,10 +2,13 @@
 import os
 import enCount.gtfs as gtfs
 import enCount.db as db
+import enCount.queues as queue
 from enCount.config import data_root, genomes_root, results_root
 import datetime
 import unittest
 import shutil
+import time
+
 
 # Mock system calls
 from mock import Mock
@@ -23,9 +26,9 @@ class TestGtfs(unittest.TestCase):
             /endata/genomes/index
     """
 
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         db.gtfs.drop()
+
 
     def test_gtf(self):
         """
@@ -42,11 +45,54 @@ class TestGtfs(unittest.TestCase):
         in_genome_fasta_dir = os.path.join(genomes_root, "fasta")
         self.assertTrue(os.path.isdir(in_genome_fasta_dir))
 
-        # The method gtfs.get_genome_index_dir is called within gtfs.generate_genome_index
-        # and shall not be called elsewhere before a genome index is generated
-        gtfs.generate_genome_index(in_gtf, in_genome_fasta_dir)
+        # Insert record into database
         self.genome_dir = gtfs.get_genome_index_dir(gtf_ver)
         self.assertTrue(os.path.isdir(self.genome_dir))
+
+        # The method gtfs.get_genome_index_dir is called within gtfs.generate_genome_index
+        # and shall not be called elsewhere before a genome index is generated
+        gtfs.generate_genome_index(in_gtf, in_genome_fasta_dir, self.genome_dir)
+        self.genome_dir = gtfs.get_genome_index_dir(gtf_ver)
+
+        # Check mapping
+        mappings = list(db.gtfs.find({"gtf_ver": gtf_ver, "status": "ready"}))
+        self.assertEqual(len(mappings), 1)
+
+
+    def test_process_queue(self):
+        """
+        Simple test of the genome index generation pipeline using the process queue.
+        """
+        self.assertTrue(db.gtfs.find().count() == 0)
+
+        # Get latest genome version
+        gtf_ver = gtfs.get_version_before(datetime.datetime.min)
+        in_gtf = os.path.join(genomes_root, "gtf", "%s.gtf" % gtf_ver)
+        self.assertTrue(os.path.exists(in_gtf))
+
+        # Process gtf_version
+        self.genome_dir = gtfs.get_genome_index_dir(gtf_ver)
+        self.assertTrue(self.genome_dir is not None)
+        mappings = list(db.gtfs.find({"gtf_ver": gtf_ver}))
+        self.assertEqual(len(mappings), 1)
+
+        # Process outstanding requests; Mock submitted jobs explicitly
+        empty = False
+        while not empty:
+            gtfs.process(mock=True)
+            empty = queue.gtfs.is_empty()
+
+        # Wait for database to refresh
+        # TODO: is there a cleaner way to ensure transactions?
+        mappings = list(db.gtfs.find({"gtf_ver": gtf_ver, "status": "ready"}))
+        while not len(mappings):
+            time.sleep(1)
+            mappings = list(db.gtfs.find({"gtf_ver": gtf_ver, "status": "ready"}))
+
+        # Make sure results exist
+        self.genome_dir = gtfs.get_genome_index_dir(gtf_ver)
+        self.assertTrue(os.path.isdir(self.genome_dir))
+
 
     def tearDown(self):
         shutil.rmtree(self.genome_dir)
