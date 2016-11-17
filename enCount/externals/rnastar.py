68f4225ed8d56e48d56f7a0328d2e3e8740c1574
mstrazar <martin.strazar@gmail.com>
2016-11-17 14:28:53 +0100

    Added genome generation parameters depending on genome length suggested by manual.

--- enCount/externals/rnastar.py
+++ enCount/externals/rnastar.py
@@ -3,6 +3,26 @@ import glob
 import os
 from enCount.config import STAR_EXEC
 from subprocess import call as sp_call
+from Bio.SeqIO import parse
+from math import log
+
+def _genome_parameters(in_genome_fasta_dir):
+    """
+    Return length of genome and number of references (chromosomes),
+    stored as a directory of fasta files. Required for calculating STAR parameters.
+
+    :param in_genome_fasta_dir:
+        Input directory with fasta genome.
+    :return:
+        Total genome length in nt.
+    """
+    genome_len = 0
+    genome_pars = 0
+    for fasta in glob.glob(os.path.join(in_genome_fasta_dir, "*.fa")):
+        for record in parse(open(fasta), format="fasta"):
+            genome_pars += 1
+            genome_len += len(record.seq)
+    return genome_len, genome_pars
 
 
 def run_star_generate_genome(in_gtf, in_genome_fasta_dir, out_genome_dir,
@@ -28,10 +48,17 @@ def run_star_generate_genome(in_gtf, in_genome_fasta_dir, out_genome_dir,
 
     tmp_dir = os.path.join(out_genome_dir, "STARtmp")
 
+    # Calculate parameters based on genome length
+    ln, refs = _genome_parameters(in_genome_fasta_dir)
+    genomeSAindexNbases = int(min(14, 0.5 * log(ln)/log(2) - 1))
+    genomeChrBinNbits = int(min(18, log(ln/refs)/log(2)))
+
     args = [STAR_EXEC, "--runThreadN", str(num_threads), "--runMode",
             "genomeGenerate", "--genomeDir", out_genome_dir, "--sjdbGTFfile",
             in_gtf, "--sjdbOverhang", str(read_length-1),
-            "--outFileNamePrefix", tmp_dir, ]
+            "--outFileNamePrefix", tmp_dir,
+            "--genomeSAindexNbases", str(genomeSAindexNbases),
+            "--genomeChrBinNbits", str(genomeChrBinNbits)]
 
     args.append("--genomeFastaFiles")
     for f in glob.glob(os.path.join(in_genome_fasta_dir, "*.fa")):
@@ -77,7 +104,7 @@ def run_star(in_fastq_pair, in_genome_dir, out_dir, num_threads=4,
             "--genomeDir",         in_genome_dir,
             "--runThreadN",        str(num_threads),
             "--outFileNamePrefix", out_dir,
-            # "--clip3pAdapterSeq",   clip3pAdapterSeq,
+            "--clip3pAdapterSeq",  clip3pAdapterSeq,
             "--outSAMtype", "BAM", "SortedByCoordinate",]
 
     # Standard ENCODE options (Manual 2.5.1, p. 7)
@@ -92,7 +119,6 @@ def run_star(in_fastq_pair, in_genome_dir, out_dir, num_threads=4,
         "--alignMatesGapMax", "1000000",
     ]
 
-
     # Process .gzip
     if in_fastq_pair[0].endswith(".gz"):
         args.append("--readFilesCommand")
@@ -100,6 +126,3 @@ def run_star(in_fastq_pair, in_genome_dir, out_dir, num_threads=4,
 
     print(" ".join(args))
     return sp_call(args)
-
-
-
