d906817673e67bf69d829f88680dfc2dac0e4570
mstrazar <martin.strazar@gmail.com>
2016-12-25 12:40:47 +0100

    Installation of R dependencies from Bioconductor/CRAN.

--- install/install.R
+++ install/install.R
@@ -2,11 +2,14 @@
 .libPaths("/home/enuser/.R/")
 sessionInfo()
 
-# Install via CRAN
-install.packages("http://hartleys.github.io/QoRTs/QoRTs_LATEST.tar.gz",
-    repos=NULL,type="source");
-
 # Install via Bioconductor
 source("https://bioconductor.org/biocLite.R")
-biocLite("JunctionSeq", ask=FALSE)
-biocLite("DEXSeq", ask=FALSE)
+biocLite()
+biocLite(c("ShortRead"))
+biocLite(c("DESeq2"))
+biocLite(c("DEXSeq"))
+biocLite(c("JunctionSeq"))
+
+# Install manually via source
+# Assume QoRTs_1.1.8.tar.gz is downloaded
+install.packages("QoRTs_1.1.8.tar.gz", repos=NULL,type="source");
